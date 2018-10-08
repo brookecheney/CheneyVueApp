d66c4933103f2d3c733764b8850d76e442ad8657
Brooklyne Cheney <brookecheney@hotmail.com>
2018-10-08 15:10:16 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <myhead/>
+    <myServerStatus/>
+    <myServerList/>
+    <myfoot/>
+  </div>
+</template>
+
+<script>
+import header from './components/header.vue'
+import footer from './components/footer.vue'
+import serverList from './components/serverList.vue'
+import serverStatus from './components/serverStatus.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'myhead': header,
+    'myfoot': footer,
+    'myServerList': serverList,
+    'myServerStatus': serverStatus
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+   <v-layout row>
+     <v-flex xs2 sm0 offset-sm0>
+     <h1>My Server </h1>
+       
+    </v-flex>
+  </v-layout></template>
+ 
+
+ 
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+
+
+<style scoped>
+
+</style>
