35e5c95e38d21130e80cd42f586f9ca59f7cb9a2
Brooklyne Cheney <brookecheney@hotmail.com>
2018-10-08 22:52:12 -0600

    Add files via upload

---
+++ src/components/serverList.vue
@@ -0,0 +1,80 @@
+<template>
+   <v-layout row>
+     <v-flex xs6 0sm offset-sm0>
+      <v-card>
+        <v-list two-line>
+          <template v-for="(item) in newList">
+            <v-list-tile
+              :key="item"
+            >
+                
+              <v-list-tile-content>
+               
+                <v-list-tile-title v-html="item.name"></v-list-tile-title>
+              <v-list-tile-title v-html="item.button"></v-list-tile-title>
+              <v-list-tile-title v-html="item.Status"></v-list-tile-title>
+
+              </v-list-tile-content>
+            </v-list-tile>
+         </template>
+
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+
+
+
+ 
+
+
+<script>
+export default {
+    data: function ()  {
+    
+    return {
+        newList: [
+            { name: 'Serve 1',
+            Status: 'Good',
+            button:  ' <button @click="changeStatus">Change Status</button>'
+},
+          { name: 'Serve 2',
+          Status: 'Bad',
+            button:  ' <button @click="changeStatus">Change Status</button>'
+           },
+            { name: 'Serve 3',
+            Status: 'Terrible',
+                        button:  ' <button @click="changeStatus">Change Status</button>'
+},
+            { name: 'Serve 4',
+            Status: 'Nice',
+                        button:  ' <button @click="changeStatus">Change Status</button>'
+},
+            { name: 'Serve 5',
+            Status: 'Okay',
+                        button:  ' <button @click="changeStatus">Change Status</button>'
+}
+            ]
+}
+}
+
+}
+
+
+
+
+
+
+
+
+</script>
+
+
+
+
+<style scoped>
+
+</style>
