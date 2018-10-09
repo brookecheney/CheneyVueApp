ea2e0415209fd9fc29800528a1760e79aa89a2b8
Brooklyne Cheney <brookecheney@hotmail.com>
2018-10-08 21:54:12 -0600

    Add files via upload

---
+++ src/components/serverStatus.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="app">
+  
+    
+       <my-cmp></my-cmp>
+
+  </div>
+  </template>
+
+ 
+
+
+<script>
+Vue.component('my-cmp',    {
+  data: function() {
+    return data;
+  },
+  template: '<p>Server Status: {{ status }} (<button@click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal' ;
+    }
+  }
+});
+</script>
+
+
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
